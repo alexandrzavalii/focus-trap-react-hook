282cbf19fc6e7b9c057533ae2a196b3333ee2030
Alex <alex.zavalii@workday.com>
2019-08-22 18:55:34 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,59 +1,16 @@
-var candidateSelectors = [
-  'input',
-  'select',
-  'textarea',
-  'a[href]',
-  'button',
-  '[tabindex]',
-  'audio[controls]',
-  'video[controls]',
-  '[contenteditable]:not([contenteditable="false"])'
-]
+import React from 'react'
+import focusTrap from 'focus-trap-js'
 
-function getAllTabbingElements (parentElem) {
-  var tabbableNodes = parentElem.querySelectorAll(candidateSelectors.join(','))
-  var filterNegativeTabIndex = []
-  for (var i = 0; i < tabbableNodes.length; i++) {
-    if (getTabindex(tabbableNodes[i]) > -1) {
-      filterNegativeTabIndex.push(tabbableNodes[i])
+const useFocusTrap = contRef => {
+  React.useEffect(() => {
+    const handleKeyEvent = event => {
+      focusTrap(event, contRef.current)
     }
-  }
-  return filterNegativeTabIndex
+    document.addEventListener('keydown', handleKeyEvent)
+    return () => {
+      document.removeEventListener('keydown', handleKeyEvent)
+    }
+  }, [contRef])
 }
 
-function tabTrappingKey (event, parentElem) {
-  // check if current event keyCode is tab
-  if (event.keyCode !== 9) return
-
-  // check if current element is inside parent element
-  if (!parentElem || !parentElem.contains(event.target)) {
-    return
-  }
-
-  var allTabbingElements = getAllTabbingElements(parentElem)
-  var firstFocusableElement = allTabbingElements[0]
-  var lastFocusableElement = allTabbingElements[allTabbingElements.length - 1]
-
-  if (event.shiftKey && event.target === firstFocusableElement) {
-    lastFocusableElement.focus()
-    event.preventDefault()
-  } else if (!event.shiftKey && event.target === lastFocusableElement) {
-    firstFocusableElement.focus()
-    event.preventDefault()
-  }
-}
-
-function getTabindex (node) {
-  var tabindexAttr = parseInt(node.getAttribute('tabindex'), 10)
-  if (!isNaN(tabindexAttr)) return tabindexAttr
-  // Browsers do not return `tabIndex` correctly for contentEditable nodes;
-  // so if they don't have a tabindex attribute specifically set, assume it's 0.
-  if (isContentEditable(node)) return 0
-  return node.tabIndex
-}
-
-function isContentEditable (node) {
-  return node.contentEditable === 'true'
-}
-
-module.exports = tabTrappingKey
+export default useFocusTrap
